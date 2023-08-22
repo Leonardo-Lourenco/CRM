e4a1fe6ff0dbb322a408a946c9969438781485e5
Leonardo Lourenco <lnddru@gmail.com>
2023-08-22 16:16:22 -0300

    Add files via upload

--- appcrm/urls.py
+++ appcrm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.contrib.auth import views
 
@@ -30,4 +30,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='userprofile/login.html'),name='login'),
     path('logout/', views.LogoutView.as_view(),name='logout'),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
 ]
