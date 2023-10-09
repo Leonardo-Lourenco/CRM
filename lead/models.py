875c8db6a40bd848184cfeae3c2ad87674e2e306
Leonardo Lourenco <lnddru@gmail.com>
2023-10-09 19:52:32 -0300

    Add files via upload

---
+++ lead/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Lead(models.Model):
+
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+    CHOICES_PRIORITY = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High')
+    )
+
+
+    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
