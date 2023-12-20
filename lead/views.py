1e0543be991c936f844eb413d29650f4e2bc6310
Leonardo Lourenco <lnddru@gmail.com>
2023-12-20 19:53:07 -0300

    Add files via upload

--- lead/views.py
+++ lead/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import AddLeadForm
+
 # Create your views here.
 @login_required
 def add_lead(request):
-    return render(request, 'lead/add_lead.html')
+
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+    form = AddLeadForm()
+
+    return render(request, 'lead/add_lead.html', {
+
+        'form': form
+
+    })
