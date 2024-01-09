ce1ff2418cd22caa596900e19a78d2fdd88be898
Leonardo Lourenco <lnddru@gmail.com>
2024-01-09 20:07:35 -0300

    Add files via upload

--- lead/urls.py
+++ lead/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-lead', views.add_lead, name='add_lead'),
+    path('add-lead/', views.add_lead, name='add_lead'),
+    path('', views.leads_list, name='leads_list'),
 ]
--- lead/views.py
+++ lead/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import AddLeadForm
+from .models import Lead
 
-# Create your views here.
+
+#LISTAR OS LEADS
+@login_required
+def leads_list(request):
+    leads = Lead.objects.filter(created_by=request.user)
+
+    return render(request, 'lead/leads_list.html', {
+        'leads': leads
+    })
+
+
+#DETAIL LEADS
+@login_required
+def leads_detail(request, pk):
+    
+    lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
+
+    return render(request, 'lead/leads_detail.html', {
+
+        'lead': lead
+
+    })
+
+
+
+
+
+# ADD LEAD
 @login_required
 def add_lead(request):
 
@@ -15,7 +43,10 @@ def add_lead(request):
             lead.created_by = request.user
             lead.save()
 
-    form = AddLeadForm()
+            return redirect('dashboard')
+
+    else:
+        form = AddLeadForm()
 
     return render(request, 'lead/add_lead.html', {
 
