40042c00a8bb4b796b1a2781debaf574e37c8e75
Leonardo Lourenco <lnddru@gmail.com>
2024-01-31 20:03:08 -0300

    Add files via upload

--- lead/urls.py
+++ lead/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-lead/', views.add_lead, name='add_lead'),
-    path('', views.leads_list, name='leads_list'),
+    path('add-lead/', views.add_lead, name='add_lead'),  ## adicionando LEADS
+    path('<int:pk>/', views.leads_detail, name='leads_detail'), ## detalhando os LEADS
+    path('', views.leads_list, name='leads_list'),   ## listando LEADS
+
+
 ]
